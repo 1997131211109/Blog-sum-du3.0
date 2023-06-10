dcac59df9d88a4926086fb3055dd85f5a5841749
denys <petrenckoden215@gmail.com>
2023-06-10 14:15:55 +0300

    Додавання SQL бази

--- blog/settings.py
+++ blog/settings.py
@@ -76,10 +76,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'your_database_name',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
